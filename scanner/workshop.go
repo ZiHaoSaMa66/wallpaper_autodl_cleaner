afd70740abcdf764013e42dcb0e17177410d2755
ZiHaoSaMa66 <ZiHaoSaMa66@users.noreply.github.com>
2026-06-11 12:38:34 +0800

    feat(wp-cleaner): workshop folder scanner

---
+++ scanner/workshop.go
@@ -0,0 +1,61 @@
+package scanner
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+func ScanWorkshop(workshopPath string) ([]uint64, error) {
+	entries, err := os.ReadDir(workshopPath)
+	if err != nil {
+		return nil, fmt.Errorf("cannot read workshop dir %s: %w", workshopPath, err)
+	}
+	var ids []uint64
+	for _, e := range entries {
+		if !e.IsDir() {
+			continue
+		}
+		id, err := strconv.ParseUint(e.Name(), 10, 64)
+		if err != nil {
+			continue
+		}
+		ids = append(ids, id)
+	}
+	return ids, nil
+}
+
+func GetWorkshopDirSize(workshopPath string) (int64, error) {
+	var total int64
+	err := filepath.WalkDir(workshopPath, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			info, err := d.Info()
+			if err == nil {
+				total += info.Size()
+			}
+		}
+		return nil
+	})
+	return total, err
+}
+
+func HumanSize(b int64) string {
+	const unit = 1024
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "KMGTPE"[exp])
+}
+
+func ParseIDsFromSubdirs(workshopPath string) ([]uint64, error) {
+	return ScanWorkshop(workshopPath)
+}
